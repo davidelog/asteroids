eaba4a3cae97007c7d4ec8df7107ca1de64ca146
davidelog <davide.sacchini01@gmail.com>
2025-02-09 00:36:43 +0100

    player created

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting asteroids!")
@@ -11,11 +12,14 @@ def main():
     fps_clock = pygame.time.Clock()
     dt = 0
     
+    user = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
     while 1>0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: #quit game with x button
                 return
         screen.fill((0, 0, 0))
+        user.draw(screen)
         pygame.display.flip()
         dt = fps_clock.tick(60) / 1000
 
