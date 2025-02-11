6c865e7d652356562ef0fe0c455e71dbb95214fb
davidelog <davide.sacchini01@gmail.com>
2025-02-11 22:52:31 +0100

    need to add collision and shooting

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     fps_clock = pygame.time.Clock()
     dt = 0
     
-    user = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    
+    AsteroidField()
+    user = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    while 1>0:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: #quit game with x button
                 return
-        screen.fill((0, 0, 0))
-        user.draw(screen)
+        updatable.update(dt)
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
         dt = fps_clock.tick(60) / 1000
 
